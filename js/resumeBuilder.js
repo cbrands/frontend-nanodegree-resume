10697badd8800b7032c7bc1bb673a25fa99138c1
charles-ordina <charles.brands@ordina.nl>
2017-08-03 10:53:51 +0200

    updated work data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -115,32 +115,25 @@ var education = {
 var work = {
     "jobs": [
         {
-            "employer": "Udacity",
-            "title": "Course Developer",
-            "location": "Mountain View, CA",
-            "dates": "Feb 2014 - Current",
-            "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+            "employer": "Rabobank",
+            "title": "Java Developer",
+            "location": "Utrecht, NL",
+            "dates": "Feb 2017 - Current",
+            "description": "Java developer. Writing java programs and doing other java stuff. Ignoring impossible demands from managers. "
         },
         {
-            "employer": "LearnBIG",
-            "title": "Software Engineer",
-            "location": "Seattle, WA",
-            "dates": "May 2013 - Jan 2014",
-            "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+            "employer": "SSC",
+            "title": "Java Developer",
+            "location": "Den Haag, NL",
+            "dates": "April 2016 - Jan 2017",
+            "description": "Java developer. Writing java programs and doing other java stuff. Ignoring impossible demands from managers. "
         },
         {
-            "employer": "LEAD Academy Charter High School",
-            "title": "Science Teacher",
-            "location": "Nashville, TN",
-            "dates": "Jul 2012 - May 2013",
-            "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
-        },
-        {
-            "employer": "Stratford High School",
-            "title": "Science Teacher",
-            "location": "Nashville, TN",
-            "dates": "Jun 2009 - Jun 2012",
-            "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+            "employer": "UWV",
+            "title": "Java Developer",
+            "location": "Amsterdam, NL",
+            "dates": "May 2014 - March 2016",
+            "description": "Java developer. Writing java programs and doing other java stuff. Ignoring impossible demands from managers. "
         }
     ],
     displayWork: function () {
